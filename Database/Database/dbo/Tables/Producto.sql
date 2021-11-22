fa7501cc0e3fbf045bd706521ff2a9e4b1aa74d2
Gamboa Porras <Gamboa Porras@GAMBOAPORRAS>
2021-11-21 23:20:27 -0600

    Add project files.

---
+++ Database/Database/dbo/Tables/Producto.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Producto]
+(
+   [IdProducto] INT NOT NULL IDENTITY(1,1) CONSTRAINT PK_Canton PRIMARY KEY CLUSTERED([IdProducto]),
+	[NombreProducto] varchar(50) NULL,
+	[PrecioProducto] int NOT NULL
+
+
+)WITH (DATA_COMPRESSION = PAGE)
+GO
---
+++ WebApp/Entity/ProductoEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class ProductoEntity
+    {
+        public int? IdProducto { get; set; }
+        public String? NombreProducto { get; set; }
+        public int? PrecioProducto { get; set; }
+    }
+}
